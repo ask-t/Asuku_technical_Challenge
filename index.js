4284ed32523199ace0a9415402dbadfd95a4ba49
ask-t <asktakahashi@go.byuh.edu>
2022-07-14 11:38:05 -1000

    added the ability to talk to google

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-const axios = require('axios');
-const aracle = require('oracledb');
-oracle.outFormat = oracle.OBJECT
-oracle.autoCommit = true;
+const axios = require('axios')
 
-const params = {
-    user: process.env.user,
-    password: process.env.password,
-    connectString: 'cman1-dev.byu.edu:31200/cescpy1.byu.edu'
-}
-
-async function testOracleConnectivity(){
-    try{
-        console.log('Testing connection to on-prem OracleDb')
-        const conn = await oracle.getConnection(params)
-        const result = await conn.execute('select * from dual ')
-        console.log(result.rows)
-        await conn.close()
-        console.log('Successfully connected to on-prem OracleDB')
-    } catch(e) {
-        console.error("Unable to create a connection to on-prem OracleDB")
-        throw e
-    }
-}
+axios.get('https://www.google.com').then(body=>{
+    console.log(body.data)
+})
---
+++ index_2.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+
+axios.get('https://www.google.com').then(body=>{
+    console.log(body.data)
+})
+
+
+
+const axios = require('axios');
+const aracle = require('oracledb');
+oracle.outFormat = oracle.OBJECT
+oracle.autoCommit = true;
+
+const params = {
+    user: process.env.user,
+    password: process.env.password,
+    connectString: 'cman1-dev.byu.edu:31200/cescpy1.byu.edu'
+}
+
+async function testOracleConnectivity(){
+    try{
+        console.log('Testing connection to on-prem OracleDb')
+        const conn = await oracle.getConnection(params)
+        const result = await conn.execute('select * from dual ')
+        console.log(result.rows)
+        await conn.close()
+        console.log('Successfully connected to on-prem OracleDB')
+    } catch(e) {
+        console.error("Unable to create a connection to on-prem OracleDB")
+        throw e
+    }
+}
