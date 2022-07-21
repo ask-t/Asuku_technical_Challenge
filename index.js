6ce7e1fdd372c14a2d210c3df21f98c18ef8979d
ask-t <asktakahashi@go.byuh.edu>
2022-07-21 10:23:02 -1000

    upload my works

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+
+
 const axios = require('axios')
 
-axios.get('https://www.google.com').then(body=>{
+const options = {
+    url: 'https://api.byu.edu:443/byuapi/persons/v3/249226495',
+    method: 'GET',
+    headers: {
+        'Authorization': 'Bearer 2bfde3c9859b4619b36b35e01528'
+    }
+}
+axios(options).then(body =>{
     console.log(body.data)
 })
