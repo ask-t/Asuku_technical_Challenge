49c6194989192e573ca34f63694102f87a9f7b30
Asuku Takahashi <107003959+ask-t@users.noreply.github.com>
2022-08-31 16:31:29 -1000

    Update token.js
    add some comments

--- token.js
+++ token.js
@@ -1,2 +1,8 @@
+/**
+ * @file it is necessary to access the API with this token.
+ * @author Asuku Takahashi
+ * Last edited: August 30, 2022 - orgnized jsdocs and add coments.
+ */
+
 const token = 'fc4Sa7d6usNC_aiSX-pRC9DEsrfJwy1uU_uA1KY3PCI.pK_Y6uln_AGmfiiAtxXd57IAGCr86V3HRllmWjQRS5c'
 exports.token = token
